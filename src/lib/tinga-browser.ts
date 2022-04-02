55c0e2013f2cf897f3c5da6614e734751a74690e
ivandotv <390700+ivandotv@users.noreply.github.com>
2022-04-02 22:41:08 +0200

    add label to tinga logger

--- src/lib/tinga-browser.ts
+++ src/lib/tinga-browser.ts
@@ -1,8 +1,9 @@
 import { browserLogLevel, browserRemoteLogLevel, remoteLogUrl } from "config"
-import { Tinga } from "tinga"
+import Tinga from "tinga"
 
 export const logger = new Tinga({
   level: browserLogLevel,
+  label: "demo",
   remote: {
     url: remoteLogUrl,
     level: browserRemoteLogLevel,
