71dc88f4647b2a927d3c5108894f7290ebb6464c
ivandotv <390700+ivandotv@users.noreply.github.com>
2022-04-04 23:37:56 +0200

    setup debug and analyze tasks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
-}
+})
 
 module.exports = nextConfig
